c1133604f3fcaba92f93702ada3e08e0175d5881
Rustam <2110151@newuu.uz>
2025-01-25 15:25:25 +0500

    clean code

---
+++ lib/features/home/presentation/cubit/category_state.dart
@@ -0,0 +1,15 @@
+
+part of 'category_cubit.dart';
+
+@immutable
+abstract class CategoryState {}
+
+
+
+class CategoryInitial extends CategoryState {}
+
+class CategorySelected extends CategoryState {
+  final String category;
+
+  CategorySelected(this.category);
+}
