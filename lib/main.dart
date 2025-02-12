f39ddafddcc70a8f83479ffa1ab1164f70d93578
Rustam <2110151@newuu.uz>
2025-02-12 22:55:13 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop/features/investment/home/presentation/screens/investment_home_screen.dart';
 import 'package:shop/features/shop/home/presentation/screens/home_page.dart';
 
 void main() {
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InvestHomePage(),
+    );
   }
 }
